a43dd16f0b791fd980af27b4b03e92602fa91a6e
cwchanap <cwchanap@connect.ust.hk>
2026-04-19 00:09:25 -0700

    chore: remove redundant foundation.dart import in parallax_terrain_layer

--- lib/game/terrain/parallax_terrain_layer.dart
+++ lib/game/terrain/parallax_terrain_layer.dart
@@ -1,6 +1,5 @@
 import 'package:flame/components.dart';
 import 'package:flame/extensions.dart';
-import 'package:flutter/foundation.dart'; // ignore: unnecessary_import
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
