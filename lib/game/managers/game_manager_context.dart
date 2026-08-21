ae5854ccf1fe35e419b396641126e8e82a3d5a40
cwchanap <cwchanap@connect.ust.hk>
2026-08-20 23:40:04 -0700

    chore: retire city runtime

--- lib/game/managers/game_manager_context.dart
+++
@@ -1,9 +0,0 @@
-import '../building/building.dart';
-import '../grid.dart';
-
-abstract interface class GameManagerContext {
-  Grid get grid;
-  Building? get buildingToPlace;
-  set buildingToPlace(Building? value);
-  void hidePlacementPreview();
-}
--- lib/game/research/index.dart
+++
@@ -1,2 +0,0 @@
-export 'research.dart';
-export 'research_type.dart';
